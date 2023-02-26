aaec8197307b05ae69115a8a0894853a713bc0fb
chwd31 <chwdwile@aol.com>
2023-02-26 10:37:19 -0600

    Started code

---
+++ server.js
@@ -0,0 +1,130 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const team = [];
+
+// Questions for the team manager
+const managerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What is the team manager's name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the team manager's employee ID?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the team manager's email address?",
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "What is the team manager's office number?",
+  },
+];
+
+// Questions for adding an engineer to the team
+const engineerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What is the engineer's name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the engineer's employee ID?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the engineer's email address?",
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is the engineer's Github username?",
+  },
+];
+
+// Questions for adding an intern to the team
+const internQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What is the intern's name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the intern's employee ID?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the intern's email address?",
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: "What is the intern's school?",
+  },
+];
+
+// Prompt the user for the team manager's information
+inquirer.prompt(managerQuestions).then((managerAnswers) => {
+  const manager = {
+    name: managerAnswers.name,
+    id: managerAnswers.id,
+    email: managerAnswers.email,
+    officeNumber: managerAnswers.officeNumber,
+  };
+  team.push(manager);
+
+  // Prompt the user for the team members' information
+  addTeamMember();
+
+  function addTeamMember() {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'memberType',
+        message: 'What type of team member do you want to add?',
+        choices: ['Engineer', 'Intern', 'Finish building my team'],
+      },
+    ]).then((memberAnswers) => {
+      if (memberAnswers.memberType === 'Engineer') {
+        inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+          const engineer = {
+            name: engineerAnswers.name,
+            id: engineerAnswers.id,
+            email: engineerAnswers.email,
+            github: engineerAnswers.github,
+          };
+          team.push(engineer);
+          addTeamMember();
+        });
+      } else if (memberAnswers.memberType === 'Intern') {
+        inquirer.prompt(internQuestions).then((internAnswers) => {
+          const intern = {
+            name: internAnswers.name,
+            id: internAnswers.id,
+            email: internAnswers.email,
+            school: internAnswers.school,
+          };
+          team.push(intern);
+          addTeamMember();
+        });
+  } else {
+        // Finish building team
+        console.log('Team roster:', team);
+        // Generate HTML page
+        // ...
+      }
+    });
+  }
+});
