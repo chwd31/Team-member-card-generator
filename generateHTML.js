bc840f93f4540b0e70270d7701eb40493e1189f8
chwd31 <chwdwile@aol.com>
2023-02-26 11:42:38 -0600

    more code

---
+++ generateHTML.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+// Load the HTML template
+const template = fs.readFileSync('template.html', 'utf8');
+
+// Compile the template
+const render = Handlebars.compile(template);
+
+// Generate the HTML file with the team data
+function generateHTML(team) {
+  const html = render({ team });
+  fs.writeFileSync('team.html', html);
+}
+
+module.exports = generateHTML;
